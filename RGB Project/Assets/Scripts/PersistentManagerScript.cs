26123393de808324fab132d9229f431fd8ad05d6
kuroki100 <leog110@gmail.com>
2018-03-30 23:23:21 -0300

    Update Pre-Alpha 0.8
    Mudanças:
    
    -Cenário do Capítulo 1 concluido.
    -Spritesheet do personagem no projeto.
    -Animações do cenário.
    -Novas plataformas e seus prefabs.
    
    Nota:
    
    -Não foi substituido os assets das cenas atuais, foi criada uma nva cena para a criação do cenário, substituir na cena principal depois.

---
+++ RGB Project/Assets/Scripts/PersistentManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentManagerScript : MonoBehaviour
+{
+
+    public static PersistentManagerScript Instance { get; private set; }
+
+    public int pontos = 0;
+    public int vida = 3;
+
+    public int pontos1 = 0;
+
+    public int pontos2 = 0;
+
+    public int pontos3 = 0;
+
+    public bool triturado = false;
+
+    public bool chegouNoFinal = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        vida = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
